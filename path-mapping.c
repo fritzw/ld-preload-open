82e99fc6bd1a37271b529bb441fc0c66d9d1290b
Niklas Rother <rother@ims.uni-hannover.de>
2022-01-19 14:46:02 +0100

    Also override various other path-related functions
    openat, openat64, fopen, fopen64, stat, lstat, opendir
    
    Props to @fritzw for the help.

--- path-mapping.c
+++ path-mapping.c
@@ -110,3 +110,118 @@ int open64(const char *pathname, int flags, ...)
     }
 }
 
+int openat(int dirfd, const char *pathname, int flags, ...)
+{
+#ifdef DEBUG
+    printf("openat(%s) called\n", pathname);
+#endif
+
+    const char *new_path = fix_path(pathname);
+
+    orig_openat_func_type orig_func;
+    orig_func = (orig_openat_func_type)dlsym(RTLD_NEXT, "openat");
+
+    // If O_CREAT is used to create a file, the file access mode must be given.
+    if (__OPEN_NEEDS_MODE(flags)) {
+        va_list args;
+        va_start(args, flags);
+        int mode = va_arg(args, int);
+        va_end(args);
+        return orig_func(dirfd, new_path, flags, mode);
+    } else {
+        return orig_func(dirfd, new_path, flags);
+    }
+}
+
+int openat64(int dirfd, const char *pathname, int flags, ...)
+{
+#ifdef DEBUG
+    printf("openat64(%s) called\n", pathname);
+#endif
+
+    const char *new_path = fix_path(pathname);
+
+    orig_openat_func_type orig_func;
+    orig_func = (orig_openat_func_type)dlsym(RTLD_NEXT, "openat64");
+
+    // If O_CREAT is used to create a file, the file access mode must be given.
+    if (__OPEN_NEEDS_MODE(flags)) {
+        va_list args;
+        va_start(args, flags);
+        int mode = va_arg(args, int);
+        va_end(args);
+        return orig_func(dirfd, new_path, flags, mode);
+    } else {
+        return orig_func(dirfd, new_path, flags);
+    }
+}
+
+FILE * fopen ( const char * filename, const char * mode )
+{
+#ifdef DEBUG
+    printf("fopen(%s) called\n", filename);
+#endif
+
+    const char *new_path = fix_path(filename);
+
+    orig_fopen_func_type orig_func;
+    orig_func = (orig_fopen_func_type)dlsym(RTLD_NEXT, "fopen");
+
+    return orig_func(new_path, mode);
+}
+
+FILE * fopen64 ( const char * filename, const char * mode )
+{
+#ifdef DEBUG
+    printf("fopen64(%s) called\n", filename);
+#endif
+
+    const char *new_path = fix_path(filename);
+
+    orig_fopen_func_type orig_func;
+    orig_func = (orig_fopen_func_type)dlsym(RTLD_NEXT, "fopen64");
+
+    return orig_func(new_path, mode);
+}
+
+int stat(const char *path, struct stat *buf)
+{
+#ifdef DEBUG
+    printf("stat(%s) called\n", path);
+#endif
+
+    const char *new_path = fix_path(path);
+
+    orig_stat_func_type orig_func;
+    orig_func = (orig_stat_func_type)dlsym(RTLD_NEXT, "stat");
+
+    return orig_func(new_path, buf);
+}
+
+int lstat(const char *path, struct stat *buf)
+{
+#ifdef DEBUG
+    printf("lstat(%s) called\n", path);
+#endif
+
+    const char *new_path = fix_path(path);
+
+    orig_stat_func_type orig_func;
+    orig_func = (orig_stat_func_type)dlsym(RTLD_NEXT, "lstat");
+
+    return orig_func(new_path, buf);
+}
+
+DIR *opendir(const char *name)
+{
+#ifdef DEBUG
+    printf("opendir(%s) called\n", name);
+#endif
+
+    const char *new_path = fix_path(name);
+
+    orig_opendir_func_type orig_func;
+    orig_func = (orig_opendir_func_type)dlsym(RTLD_NEXT, "opendir");
+
+    return orig_func(new_path);
+}
